7a0ec1f424b7352343ff32ac9a8715615c2cc6e9
Patrick Schlaepfer <patrick@schlaepfer.com>
2016-08-15 23:45:46 +0200

    Added scripts

---
+++ docker-push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker push pschlae/patient-config-service:latest
+docker push pschlae/patient-discovery-service:latest
+docker push pschlae/patient-gateway-service:latest
+docker push pschlae/patient-query-side:latest
+docker push pschlae/patient-command-side:latest
